fa7356d2d40d692bc09665e082cb49706ff97148
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-11-01 05:17:47 +0600

    Database connection established

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hwr1u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('Database connected!');
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
     res.send('VROMON server is running...');
